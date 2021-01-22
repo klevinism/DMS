5e32f9b22a89f5e631487bb8f9c0a20b5b63f4f6
klevin.delimeta <klevin.delimeta@moveoalbania.com>
2021-01-22 13:42:39 +0100

    Fix wrong naming of model controller impl and interface

--- src/main/java/com/visionous/dms/model/ModelController.java
+++ src/main/java/com/visionous/dms/model/ModelController.java
@@ -3,7 +3,6 @@
  */
 package com.visionous.dms.model;
 
-import java.util.HashMap;
 import java.util.Map;
 
 import org.springframework.ui.Model;
@@ -12,123 +11,44 @@ import org.springframework.ui.Model;
  * @author delimeta
  *
  */
-public class ModelController implements ModelControllerImpl{
+public interface ModelController {
+	/**
+	 * A custom implementation of model view mappings. 
+	 */
+	void run();
 	
 	/**
-	 * 
+	 * @return ModelControllerImpl
 	 */
-	private Map<String, Object> paramMap;
-	private Model viewModel;
+	ModelController addControllerParam(String key, Object value);
 	
+	/**
+	 * @return ModelControllerImpl
+	 */
+	ModelController removeControllerParam(String key);
+	
+	/**
+	 * @return ModelControllerImpl
+	 */
+	ModelController setControllerParam(String key, Object newValue);
+
+
+	/**
+	 * @return HashMap<String, Object>
+	 */
+	Map<String, Object> getAllControllerParams();
+
+
+	/**
+	 * @param <T>
+	 * @param object
+	 * @return
+	 */
+	<T> ModelController addModelAttributes(T object);
+
 	/**
 	 * @param model
+	 * @return
 	 */
-	public ModelController(Model model) {
-		this.viewModel = model;
-		paramMap = new HashMap<>();
-	}
-	
-	/**
-	 * Default Constructor
-	 */
-	public ModelController() {
-		paramMap = new HashMap<>();
-	}
-	
-	
-	/**
-	 * @param key String
-	 * @param value Object
-	 * @return this ModelController
-	 */
-	protected ModelController addModelCollectionToView(String key, Object value) {
-		this.viewModel.addAttribute(key,value);
-		return this;
-	}
-	
-	public Object getModelCollectionToView(String key) {
-		return this.viewModel.getAttribute(key);
-	}
-	
-	/**
-	 * 
-	 */
-	public ModelController setViewModel(Model model) {
-		this.viewModel = model;
-		return this;
-	}
-	
-	/**
-	 * Clears the model parameters of the current {@link ModelController}. 
-	 * @return this ModelController
-	 */
-	public ModelController init() {
-		this.paramMap.clear();
-		return this;
-	}
-
-	/**
-	 * 
-	 */
-	public void run() {
-		/*
-		 * Method will be overridden by subclasses
-		 * For quick example ONLY, uncomment the line below and a test parameter will be
-		 * added to the html page shown.
-		 * 
-		 * addCollectionToView("testParameter", "Hello World");
-		 */
-	}
-	
-	/**
-	 * @return this ModelController
-	 */
-	@Override
-	public ModelController addControllerParam(String key, Object value) {
-		if(value != null && !value.equals("")
-				&& key != null && !key.equals("")) paramMap.put(key, value) ;
-		return this;
-	}
-
-	/**
-	 * @return this ModelController
-	 */
-	@Override
-	public ModelController removeControllerParam(String key) {
-		if(key != null && !key.equals("")) 
-			paramMap.remove(key);
-		return this;
-	}
-
-	/**
-	 * Replaces an already present model parameter value with a newValue
-	 * @param key The String parameter key
-	 * @param newValue The new value of the parameter to replace
-	 * @return this {@link ModelController} object
-	 */
-	@Override
-	public ModelController setControllerParam(String key, Object newValue) {
-		if(newValue != null && !newValue.equals("")
-				&& key != null && !key.equals("")) 
-			paramMap.replace(key, paramMap.get(key), newValue);
-		return this;
-	}
-	
-	/**
-	 * @return paramMap Map<String, Object>
-	 */
-	@Override
-	public Map<String, Object> getAllControllerParams(){
-		return this.paramMap;
-	}
-	
-	/**
-	 * Add model to collection.
-	 */
-	@Override
-	public <T> ModelControllerImpl addModelAttributes(T object) {
-		addModelCollectionToView(object.getClass().getSimpleName(), object);
-		return this;
-	}
-	
+	ModelController setViewModel(Model model);
 }
--- src/main/java/com/visionous/dms/model/ModelControllerImpl.java
+++ src/main/java/com/visionous/dms/model/ModelControllerImpl.java
@@ -3,6 +3,7 @@
  */
 package com.visionous.dms.model;
 
+import java.util.HashMap;
 import java.util.Map;
 
 import org.springframework.ui.Model;
@@ -11,44 +12,123 @@ import org.springframework.ui.Model;
  * @author delimeta
  *
  */
-public interface ModelControllerImpl {
-	/**
-	 * A custom implementation of model view mappings. 
-	 */
-	void run();
+public class ModelControllerImpl implements ModelController{
 	
 	/**
-	 * @return ModelControllerImpl
+	 * 
 	 */
-	ModelControllerImpl addControllerParam(String key, Object value);
+	private Map<String, Object> paramMap;
+	private Model viewModel;
 	
-	/**
-	 * @return ModelControllerImpl
-	 */
-	ModelControllerImpl removeControllerParam(String key);
-	
-	/**
-	 * @return ModelControllerImpl
-	 */
-	ModelControllerImpl setControllerParam(String key, Object newValue);
-
-
-	/**
-	 * @return HashMap<String, Object>
-	 */
-	Map<String, Object> getAllControllerParams();
-
-
-	/**
-	 * @param <T>
-	 * @param object
-	 * @return
-	 */
-	<T> ModelControllerImpl addModelAttributes(T object);
-
 	/**
 	 * @param model
-	 * @return
 	 */
-	ModelControllerImpl setViewModel(Model model);
+	public ModelControllerImpl(Model model) {
+		this.viewModel = model;
+		paramMap = new HashMap<>();
+	}
+	
+	/**
+	 * Default Constructor
+	 */
+	public ModelControllerImpl() {
+		paramMap = new HashMap<>();
+	}
+	
+	
+	/**
+	 * @param key String
+	 * @param value Object
+	 * @return this ModelController
+	 */
+	protected ModelControllerImpl addModelCollectionToView(String key, Object value) {
+		this.viewModel.addAttribute(key,value);
+		return this;
+	}
+	
+	public Object getModelCollectionToView(String key) {
+		return this.viewModel.getAttribute(key);
+	}
+	
+	/**
+	 * 
+	 */
+	public ModelControllerImpl setViewModel(Model model) {
+		this.viewModel = model;
+		return this;
+	}
+	
+	/**
+	 * Clears the model parameters of the current {@link ModelControllerImpl}. 
+	 * @return this ModelController
+	 */
+	public ModelControllerImpl init() {
+		this.paramMap.clear();
+		return this;
+	}
+
+	/**
+	 * 
+	 */
+	public void run() {
+		/*
+		 * Method will be overridden by subclasses
+		 * For quick example ONLY, uncomment the line below and a test parameter will be
+		 * added to the html page shown.
+		 * 
+		 * addCollectionToView("testParameter", "Hello World");
+		 */
+	}
+	
+	/**
+	 * @return this ModelController
+	 */
+	@Override
+	public ModelControllerImpl addControllerParam(String key, Object value) {
+		if(value != null && !value.equals("")
+				&& key != null && !key.equals("")) paramMap.put(key, value) ;
+		return this;
+	}
+
+	/**
+	 * @return this ModelController
+	 */
+	@Override
+	public ModelControllerImpl removeControllerParam(String key) {
+		if(key != null && !key.equals("")) 
+			paramMap.remove(key);
+		return this;
+	}
+
+	/**
+	 * Replaces an already present model parameter value with a newValue
+	 * @param key The String parameter key
+	 * @param newValue The new value of the parameter to replace
+	 * @return this {@link ModelControllerImpl} object
+	 */
+	@Override
+	public ModelControllerImpl setControllerParam(String key, Object newValue) {
+		if(newValue != null && !newValue.equals("")
+				&& key != null && !key.equals("")) 
+			paramMap.replace(key, paramMap.get(key), newValue);
+		return this;
+	}
+	
+	/**
+	 * @return paramMap Map<String, Object>
+	 */
+	@Override
+	public Map<String, Object> getAllControllerParams(){
+		return this.paramMap;
+	}
+	
+	/**
+	 * Add model to collection.
+	 */
+	@Override
+	public <T> ModelController addModelAttributes(T object) {
+		addModelCollectionToView(object.getClass().getSimpleName(), object);
+		return this;
+	}
+	
 }
